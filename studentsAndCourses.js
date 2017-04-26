639f9dcda5504908bf1e7c699a853c4780ad3a17
App Academy Student <student@appacademy.io>
2017-04-26 16:44:15 -0700

    Finish phase 2 of studentsAndCourses

--- studentsAndCourses.js
+++ studentsAndCourses.js
@@ -9,11 +9,24 @@ Student.prototype.name = function() {
 };
 
 Student.prototype.enroll = function(course) {
-  if (!this.courseList.includes(course)) {
+  if (!this.courseList.includes(course) && !this.isConflict(course)) {
     this.courseList.push(course);
     course.students.push(this);
+  } else {
+    throw "This course conflicts with the current courses!";
   }
 };
+
+
+Student.prototype.isConflict = function(course) {
+  this.courseList.forEach( enrolledCourse => {
+    if (enrolledCourse.conflictsWith(course)) {
+      return true;
+    }
+  });
+  return false;
+};
+
 
 Student.prototype.courseLoad = function() {
   let load = {};
@@ -31,13 +44,29 @@ Student.prototype.courseLoad = function() {
   return load;
 };
 
-function Course(name, department, credits) {
+function Course(name, department, credits, block, days) {
   this.name = name;
   this.department = department;
   this.credits = credits;
   this.students = [];
+  this.block = block;
+  this.days = days;
 }
 
 Course.prototype.addStudent = function(student) {
   student.enroll(this);
+};
+
+Course.prototype.conflictsWith = function(course) {
+  if (this.block !== course.block) {
+    return false;
+  }
+
+  this.days.forEach( day => {
+    if (course.days.includes(day)) {
+      return true;
+    }
+  });
+
+  return false;
 };
