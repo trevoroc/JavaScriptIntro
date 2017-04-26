f9ac0e64b9c2128dfbaba041d75c5ce4216098fc
App Academy Student <student@appacademy.io>
2017-04-26 11:22:50 -0700

    Add Array.prototype.twoSum

--- array.js
+++ array.js
@@ -13,3 +13,19 @@ Array.prototype.uniq = function() {
 };
 
 // [1, 2, 1, 3, 3].uniq();
+
+Array.prototype.twoSum = function () {
+  let twoSums = [];
+
+  for (let i = 0; i < this.length - 1; i++) {
+    for (let j = i + 1; j < this.length; j++ ) {
+      if (this[i] + this[j] === 0) {
+        twoSums.push([i, j]);
+      }
+    }
+  }
+
+  return twoSums;
+};
+
+[-1, 0, 2, -2, 1].twoSum();
